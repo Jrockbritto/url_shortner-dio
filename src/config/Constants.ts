811c82aea397682695cac86b4624eb1052848a46
Joao Rock <50022149+Jrockbritto@users.noreply.github.com>
2022-03-23 19:20:32 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
     API_URL: 'http://localhost:5000',
-    MONGO_CONNECION: 'mongodb+srv://admin:admin@urlshortner-dio.gdcxc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MONGO_CONNECION: ''
 }
